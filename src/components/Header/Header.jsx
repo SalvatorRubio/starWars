7627b1f71c28702e559deefd6a1d2d3c800fdeab
Matvey <matvejilchenko9388@gmail.com>
2022-10-11 19:43:43 +0300

    Добавил иконки и функционал на изменение их

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import {
+  useTheme,
+  THEME_LIGHT,
+  THEME_DARK,
+  THEME_NEITRAL,
+} from "@context/ThemeProvider";
 
 import Favorite from "@components/Favorite/Favorite";
+
+import imgDroid from "./img/droid.svg";
+import imgLightSaber from "./img/lightsaber.svg";
+import imgStarDeath from "./img/spacestation.svg";
 
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const [icon, setIcon] = useState(imgStarDeath);
+  const isTheme = useTheme();
+
+  useEffect(() => {
+    switch (isTheme.theme) {
+      case THEME_LIGHT:
+        setIcon(imgLightSaber);
+        break;
+      case THEME_DARK:
+        setIcon(imgStarDeath);
+        break;
+      case THEME_NEITRAL:
+        setIcon(imgDroid);
+        break;
+
+      default:
+        setIcon(imgStarDeath);
+        break;
+    }
+  }, [isTheme]);
+
   return (
     <div className={styles.container}>
+      <img className={styles.icon} src={icon} alt="Icon" />
       <ul className={styles.list__container}>
         <li>
           <NavLink to="/">Home</NavLink>
